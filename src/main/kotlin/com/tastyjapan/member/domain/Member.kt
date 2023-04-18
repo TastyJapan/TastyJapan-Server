cdf97eb85f57b007637c954dea512f43261536c8
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-04-18 16:01:05 +0900

    Feat Member 엔티티 추가

---
+++ src/main/kotlin/com/tastyjapan/member/domain/Member.kt
@@ -0,0 +1,23 @@
+package com.tastyjapan.member.domain
+
+import lombok.NoArgsConstructor
+import javax.persistence.*
+
+@Entity
+@NoArgsConstructor
+class Member(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    val id: Long,
+
+    @Column(name="member_name")
+    val name: String,
+
+    @Column(name = "member_email")
+    val email: String,
+
+    @Enumerated(EnumType.STRING)
+    val role: Role
+    ) {
+}
