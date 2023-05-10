e94da571779f22a2238e50ce13d4856b16d55195
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-05-11 00:46:10 +0900

    feat: data class로 수정

--- src/main/kotlin/com/tastyjapan/member/domain/Member.kt
+++ src/main/kotlin/com/tastyjapan/member/domain/Member.kt
@@ -3,16 +3,17 @@ package com.tastyjapan.member.domain
 import com.tastyjapan.group.domain.Group
 import com.tastyjapan.oauth.domain.Oauth
 import com.tastyjapan.review.domain.Review
+import lombok.Getter
 import lombok.NoArgsConstructor
 import javax.persistence.*
 
 @Entity
 @NoArgsConstructor
-class Member(
+data class Member(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
-    val id: Long,
+    val id: Long? = null,
 
     @Column(name="member_name")
     val name: String,
@@ -26,14 +27,17 @@ class Member(
     @Enumerated(EnumType.STRING)
     val role: Role,
     ) {
-
+    /**
+     * 연관 관계 메서드
+     */
     @OneToMany(mappedBy = "member")
     var groups: MutableList<Group> = mutableListOf()
 
     @OneToMany(mappedBy = "member")
-    val reviews: MutableList<Review> = mutableListOf()
+    var reviews: MutableList<Review> = mutableListOf()
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "oauth_id")
     lateinit var oauth: Oauth
+
 }
