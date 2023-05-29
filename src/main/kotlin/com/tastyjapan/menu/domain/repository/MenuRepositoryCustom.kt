08e517e1cf3a9061ef64fffa7565c109604b49a7
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-05-29 15:38:55 +0900

    feat: Menu 조회 기능

--- src/main/kotlin/com/tastyjapan/menu/domain/repository/MenuRepositoryCustom.kt
+++ src/main/kotlin/com/tastyjapan/menu/domain/repository/MenuRepositoryCustom.kt
@@ -3,4 +3,5 @@ package com.tastyjapan.menu.domain.repository
 import com.tastyjapan.menu.domain.Menu
 
 interface MenuRepositoryCustom {
+    fun findMenuByRestaurantId(restaurantId: Long): List<Menu>
 }
--- src/main/kotlin/com/tastyjapan/menu/domain/repository/MenuRepositoryImpl.kt
+++ src/main/kotlin/com/tastyjapan/menu/domain/repository/MenuRepositoryImpl.kt
@@ -8,4 +8,12 @@ import com.tastyjapan.restaurant.domain.QRestaurant.restaurant
 
 class MenuRepositoryImpl(private val entityManager: EntityManager) : MenuRepositoryCustom {
     private val queryFactory: JPAQueryFactory = JPAQueryFactory(entityManager)
+
+    override fun findMenuByRestaurantId(restaurantId: Long): List<Menu> {
+        return queryFactory
+            .selectFrom(menu)
+            .leftJoin(menu.restaurant, restaurant)
+            .where(restaurant.id.eq(restaurantId))
+            .fetch()
+    }
 }
