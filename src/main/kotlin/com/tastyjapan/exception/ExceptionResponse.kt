b5e7a342c18411457dbadf61f0deac88f8f3e142
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-05-29 16:59:05 +0900

    chore: application.yml 유지보수용 코드 추가

--- src/main/kotlin/com/tastyjapan/exception/ExceptionResponse.kt
+++ src/main/kotlin/com/tastyjapan/exception/ExceptionResponse.kt
@@ -4,5 +4,5 @@ import lombok.Getter
 
 @Getter
 class ExceptionResponse (var errorCode: String, var message: String){
-
+    constructor(errorType: ErrorType) : this(errorType.errorCode, errorType.message)
 }
